84e1ed440777688e4246f08a5babb7d5fdd56bfa
Raj-Singh08 <vimal38051@gmail.com>
2025-08-30 18:15:06 +0530

    Model function:easir way to setup two way binding between one companent and anothers template and same template too

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, Output } from '@angular/core';
+import { Component, EventEmitter, input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,11 +9,15 @@ import { Component, EventEmitter, input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  size = input.required<{width:string,height:string}>();
-  @Output() sizeChange = new EventEmitter<{width:string,height:string}>();  //name should be like this only"sizeChange" inputname+Chnage
+  //size = input.required<{width:string,height:string}>();
+//  @Output() sizeChange = new EventEmitter<{width:string,height:string}>();  //name should be like this only"sizeChange" inputname+Chnage
   //to create two way bindable 
 
+  size = model.required<{width:string,height:string}>(); //easier way to make custom two binding. this model function will make this size two way binding
+  //No need to create seprate input and output
+
   onReset() {
-    this.sizeChange.emit({width:'200',height:'100'});
+    //this.sizeChange.emit({width:'200',height:'100'});
+    this.size.set({width:'100',height:'200'});
   }
 }
